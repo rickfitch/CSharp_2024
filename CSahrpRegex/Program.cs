aa1ec6173b8e20319c884d8d23feb658d08721de
rickfitch <rickfitch@users.noreply.github.com>
2023-11-15 21:58:27 -0800

    DateTimeClass examples

--- CSahrpRegex/Program.cs
+++ CSahrpRegex/Program.cs
@@ -6,14 +6,14 @@ namespace CSharpRegex
     {
         static void Main(string[] args)
         {
-            Regex regex = new Regex(@"\d");
+            Regex regex = new Regex(@"\d\d$"); //last two digits (56)
 
             string text = "Hi there my number is 195256";
 
             MatchCollection matches = regex.Matches(text);
             foreach (Match match in matches) 
             {
-                Console.WriteLine("count {0}\t match found {1}", match.Index, match);
+                Console.WriteLine("count {0} \t match found {1}", match.Index, match);
             }
 
             Console.ReadKey();
---
+++ DateTimeClass/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace DateTimeClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(DateTime.UtcNow);
+
+            Console.WriteLine("Today is {0}", DateTime.Today.DayOfWeek);
+            Console.WriteLine("Tomorrow is {0}", DateTime.Today.AddDays(1).DayOfWeek);
+
+            DateTime now = DateTime.Now;
+            Console.WriteLine("it is {0} o'clock, {1} minutes and {2} seconds.", now.Hour, now.Minute, now.Second);
+
+            Console.WriteLine("write a date in this format: yyyy-mm-dd");
+            string input = Console.ReadLine();
+
+            if(DateTime.TryParse(input, out DateTime date))
+            {
+                Console.WriteLine("date is: {0}", date);
+                Console.WriteLine("time past: {0}",DateTime.Now.Subtract(date));
+                TimeSpan timeSpan = now.Subtract(date);
+                Console.WriteLine(timeSpan.Days.ToString());
+
+            }
+            else { Console.WriteLine("improper date"); }
+
+
+            Console.ReadKey();
+        }
+    }
+}
