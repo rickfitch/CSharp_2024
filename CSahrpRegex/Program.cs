5f02d0bad7c82c9498db46eaa97241f06f585047
rickfitch <rickfitch@users.noreply.github.com>
2023-11-15 18:49:18 -0800

    regex expressions and cheat sheet

---
+++ CSahrpRegex/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CSharpRegex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"\d");
+
+            string text = "Hi there my number is 195256";
+
+            MatchCollection matches = regex.Matches(text);
+            foreach (Match match in matches) 
+            {
+                Console.WriteLine("count {0}\t match found {1}", match.Index, match);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
