d37a03846730ee3fe860b6a68735fe6351fcb553
rickfitch <rickfitch@hotmail.com>
2025-07-27 11:01:29 -0700

    udemy threading course in C# added. 07/28/2025

---
+++ threading01/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace threading01
+{
+    internal class Program
+    {
+        static void WriteThreadID()
+        {
+            // This method is intended to be run in a separate thread
+            Console.WriteLine($"Thread ID: {Environment.CurrentManagedThreadId}");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            // Create a new thread to run the WriteThreadID method
+            WriteThreadID();
+            Thread thread01 = new Thread(WriteThreadID);
+            thread01.Start();
+            Console.ReadLine();
+        }
+
+       
+    }
+}
