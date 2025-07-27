3d2565e1cbedee4f420fa81d4b334264b8aa6f49
rickfitch <rickfitch@hotmail.com>
2025-07-27 11:59:17 -0700

    added thread priority with and w/o thread sleep

--- threading01/Program.cs
+++ threading01/Program.cs
@@ -5,15 +5,34 @@
         static void WriteThreadID()
         {
             // This method is intended to be run in a separate thread
-            Console.WriteLine($"Thread ID: {Environment.CurrentManagedThreadId}");
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name);
+                // Simulate some work slows down the output
+                Thread.Sleep(50);//priority is not guaranteed to be honored by the OS when
+                                // running on a single core CPU.
+            }
+
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            // Create a new thread to run the WriteThreadID method
-            WriteThreadID();
-            Thread thread01 = new Thread(WriteThreadID);
+            //Console.WriteLine($"Main Thread ID: {Environment.CurrentManagedThreadId}");
+            
+            Thread thread01 = new Thread(WriteThreadID);            
+            Thread thread02 = new Thread(WriteThreadID);
+            thread01.Name = "Thread 01";
+            thread02.Name = "Thread 02";
+            thread01.Priority = ThreadPriority.Highest;
+            thread02.Priority = ThreadPriority.Lowest;
+            Thread.CurrentThread.Priority = ThreadPriority.Normal;
+            Thread.CurrentThread.Name = "Main Thread";
+
+            // Start the threads
+            // Create and start two additional threads. Not the main thread.
             thread01.Start();
+            thread02.Start();
+            WriteThreadID();//This runs in the main thread. 
             Console.ReadLine();
         }
 
